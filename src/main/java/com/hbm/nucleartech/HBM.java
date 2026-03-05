303b24e6b816a84919214904410c70dc99af2bd5
benjaminnautilus <benjaminsnautilus@gmail.com>
2026-03-05 14:15:18 -0500

    Refactor imports and improve code organization

--- src/main/java/com/hbm/nucleartech/HBM.java
+++ src/main/java/com/hbm/nucleartech/HBM.java
@@ -1,7 +1,6 @@
 package com.hbm.nucleartech;
 
 import com.hbm.nucleartech.block.RegisterBlocks;
-import com.hbm.nucleartech.block.custom.ArmorModificationTableBlock;
 import com.hbm.nucleartech.block.entity.RegisterBlockEntities;
 import com.hbm.nucleartech.block.entity.client.*;
 import com.hbm.nucleartech.entity.HbmEntities;
@@ -15,72 +14,103 @@ import com.hbm.nucleartech.item.custom.GeigerCounterItem;
 import com.hbm.nucleartech.network.HbmPacketHandler;
 import com.hbm.nucleartech.particle.RegisterParticles;
 import com.hbm.nucleartech.recipe.RegisterRecipes;
-import com.hbm.nucleartech.screen.ArmorModificationTableScreen;
-import com.hbm.nucleartech.screen.BurnerPressScreen;
-import com.hbm.nucleartech.screen.RegisterMenuTypes;
-import com.hbm.nucleartech.screen.ShredderScreen;
+import com.hbm.nucleartech.screen.*;
 import com.hbm.nucleartech.sound.RegisterSounds;
 import com.hbm.nucleartech.util.ArmorUtil;
 import com.hbm.nucleartech.util.InterceptUtil;
 import com.mojang.logging.LogUtils;
+
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.client.renderer.entity.EntityRenderers;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
+
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.*;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.*;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
-import net.minecraftforge.network.NetworkEvent;
-import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.*;
 import net.minecraftforge.network.simple.SimpleChannel;
 import net.minecraftforge.registries.ForgeRegistries;
+
 import org.slf4j.Logger;
 import software.bernie.geckolib.GeckoLib;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.*;
+import java.util.function.*;
 
-import static com.hbm.nucleartech.block.RegisterBlocks.HAZARD_BLOCKS;
-
-// The value here should match an entry in the META-INF/mods.toml file
+/**
+ * Main mod entrypoint.
+ */
 @Mod(HBM.MOD_ID)
-public class HBM
-{
-    // Define mod id in a common place for everything to reference
+public class HBM {
+
+    /* ------------------------------------------------------------ */
+    /* Constants                                                     */
+    /* ------------------------------------------------------------ */
+
     public static final String MOD_ID = "hbm";
-    // Directly reference a slf4j logger
+    private static final String PROTOCOL_VERSION = "1";
+
     public static final Logger LOGGER = LogUtils.getLogger();
+    private static final RandomSource RANDOM = RandomSource.create();
+
+    /* ------------------------------------------------------------ */
+    /* Network                                                       */
+    /* ------------------------------------------------------------ */
+
+    public static final SimpleChannel PACKET_HANDLER = NetworkRegistry.newSimpleChannel(
+            new ResourceLocation(MOD_ID, MOD_ID),
+            () -> PROTOCOL_VERSION,
+            PROTOCOL_VERSION::equals,
+            PROTOCOL_VERSION::equals
+    );
+
+    private static int messageID = 0;
+
+    /* ------------------------------------------------------------ */
+    /* Config                                                        */
+    /* ------------------------------------------------------------ */
 
     public static File configDir;
     public static File configHbmDir;
 
-    public HBM() {
+    /* ------------------------------------------------------------ */
+    /* Constructor                                                   */
+    /* ------------------------------------------------------------ */
 
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    public HBM() {
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        initializeLibraries();
+        registerContent(modEventBus);
+        registerEvents(modEventBus);
+        registerConfig();
+    }
+
+    /* ------------------------------------------------------------ */
+    /* Initialization                                                */
+    /* ------------------------------------------------------------ */
+
+    private void initializeLibraries() {
+        GeckoLib.initialize();
+    }
+
+    private void registerContent(IEventBus modEventBus) {
 
         InterceptUtil.registerVanillaItemIntercepts(modEventBus);
-
-        GeckoLib.initialize();
 
         RegisterCreativeTabs.register(modEventBus);
         RegisterItems.register(modEventBus);
@@ -88,69 +118,74 @@ public class HBM
         RegisterFluids.register(modEventBus);
         RegisterParticles.register(modEventBus);
         HbmEntities.register(modEventBus);
-//        ClientSetup.init(modEventBus);
         RegisterBlockEntities.register(modEventBus);
         RegisterMenuTypes.register(modEventBus);
-
         RegisterRecipes.register(modEventBus);
 
         RegisterSounds.SOUNDS.register(modEventBus);
+    }
 
-        // Register the commonSetup method for modloading
+    private void registerEvents(IEventBus modEventBus) {
+
         modEventBus.addListener(this::commonSetup);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void registerConfig() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private static final String PROTOCOL_VERSION = "1";
-    public static final SimpleChannel PACKET_HANDLER = NetworkRegistry.newSimpleChannel(new ResourceLocation(MOD_ID, MOD_ID),
-            () -> PROTOCOL_VERSION, PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals);
-    private static int messageID = 0;
+    /* ------------------------------------------------------------ */
+    /* Networking                                                    */
+    /* ------------------------------------------------------------ */
 
     public static <T> void addNetworkMessage(
             Class<T> messageType,
             BiConsumer<T, FriendlyByteBuf> encoder,
             Function<FriendlyByteBuf, T> decoder,
-            BiConsumer<T, Supplier<NetworkEvent.Context>> messageConsumer) {
-
-        PACKET_HANDLER.registerMessage(messageID, messageType, encoder, decoder, messageConsumer);
-        messageID++;
+            BiConsumer<T, Supplier<NetworkEvent.Context>> messageConsumer
+    ) {
+        PACKET_HANDLER.registerMessage(messageID++, messageType, encoder, decoder, messageConsumer);
     }
+
+    /* ------------------------------------------------------------ */
+    /* Setup                                                         */
+    /* ------------------------------------------------------------ */
 
     private void commonSetup(final FMLCommonSetupEvent event) {
 
-        event.enqueueWork(() -> {
+        event.enqueueWork(() ->
+                net.minecraft.world.item.Items.COAL = InterceptUtil.COAL_REPLACEMENT.get()
+        );
 
-            net.minecraft.world.item.Items.COAL = InterceptUtil.COAL_REPLACEMENT.get();
-        });
-
-        configDir = FMLPaths.CONFIGDIR.get().toFile();
-        configHbmDir = new File(configDir.getAbsolutePath() + File.separatorChar + "hbmConfig");
-
-        if(!configHbmDir.exists())
-            configHbmDir.mkdir();
+        initializeConfigDirectory();
 
         HbmPacketHandler.register();
         HazmatRegistry.registerHazmats();
         ArmorUtil.register();
-
         GeigerCounterItem.initSoundMap();
     }
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+    private static void initializeConfigDirectory() {
 
+        configDir = FMLPaths.CONFIGDIR.get().toFile();
+        configHbmDir = new File(configDir, "hbmConfig");
 
+        if (!configHbmDir.exists()) {
+            configHbmDir.mkdirs();
+        }
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    /* ------------------------------------------------------------ */
+    /* Events                                                        */
+    /* ------------------------------------------------------------ */
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        // Intentionally empty
+    }
+
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
 
@@ -158,38 +193,72 @@ public class HBM
         HbmContaminationSystem.init(event.getServer());
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    /* ------------------------------------------------------------ */
+    /* Client                                                        */
+    /* ------------------------------------------------------------ */
+
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
 
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
 
-            EntityRenderers.register(HbmEntities.NUCLEAR_CREEPER.get(), NuclearCreeperRenderer::new);
-            BlockEntityRenderers.register(RegisterBlockEntities.BURNER_PRESS_ENTITY.get(), BurnerPressRenderer::new);
-            BlockEntityRenderers.register(RegisterBlockEntities.SHREDDER_ENTITY.get(), ShredderRenderer::new);
-            BlockEntityRenderers.register(RegisterBlockEntities.GRAPHITE_BLOCK_ENTITY.get(), GraphiteBlockRenderer::new);
-            BlockEntityRenderers.register(RegisterBlockEntities.ARMOR_MODIFICATION_TABLE_ENTITY.get(), ArmorModificationTableRenderer::new);
+            EntityRenderers.register(
+                    HbmEntities.NUCLEAR_CREEPER.get(),
+                    NuclearCreeperRenderer::new
+            );
 
-            MenuScreens.register(RegisterMenuTypes.BURNER_PRESS_MENU.get(), BurnerPressScreen::new);
-            MenuScreens.register(RegisterMenuTypes.SHREDDER_MENU.get(), ShredderScreen::new);
-            MenuScreens.register(RegisterMenuTypes.ARMOR_MODIFICATION_TABLE_MENU.get(), ArmorModificationTableScreen::new);
+            BlockEntityRenderers.register(
+                    RegisterBlockEntities.BURNER_PRESS_ENTITY.get(),
+                    BurnerPressRenderer::new
+            );
+
+            BlockEntityRenderers.register(
+                    RegisterBlockEntities.SHREDDER_ENTITY.get(),
+                    ShredderRenderer::new
+            );
+
+            BlockEntityRenderers.register(
+                    RegisterBlockEntities.GRAPHITE_BLOCK_ENTITY.get(),
+                    GraphiteBlockRenderer::new
+            );
+
+            BlockEntityRenderers.register(
+                    RegisterBlockEntities.ARMOR_MODIFICATION_TABLE_ENTITY.get(),
+                    ArmorModificationTableRenderer::new
+            );
+
+            MenuScreens.register(
+                    RegisterMenuTypes.BURNER_PRESS_MENU.get(),
+                    BurnerPressScreen::new
+            );
+
+            MenuScreens.register(
+                    RegisterMenuTypes.SHREDDER_MENU.get(),
+                    ShredderScreen::new
+            );
+
+            MenuScreens.register(
+                    RegisterMenuTypes.ARMOR_MODIFICATION_TABLE_MENU.get(),
+                    ArmorModificationTableScreen::new
+            );
         }
     }
 
-    private static final RandomSource RANDOM = RandomSource.create();
+    /* ------------------------------------------------------------ */
+    /* Utilities                                                     */
+    /* ------------------------------------------------------------ */
 
     public static int random(int min, int max) {
-
         return RANDOM.nextInt(max - min + 1) + min;
     }
 
     public static List<Item> getItemsFromTag(TagKey<Item> tagKey) {
+
         List<Item> items = new ArrayList<>();
 
-        // ForgeRegistries.ITEMS is wrapped with holders
-        for (Item holder : ForgeRegistries.ITEMS.tags().getTag(tagKey)) {
-            items.add(holder);
+        for (Item item : ForgeRegistries.ITEMS.tags().getTag(tagKey)) {
+            items.add(item);
         }
 
         return items;
